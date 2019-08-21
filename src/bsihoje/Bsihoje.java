97a58e5ea284409e98ea65ccc4bce2b51d5ecbba
Laboratorio <Laboratorio@LAB00XX.mshome.net>
2019-08-21 20:30:03 -0300

    Criei janela principal

--- src/bsihoje/Bsihoje.java
+++ src/bsihoje/Bsihoje.java
@@ -15,7 +15,8 @@ public class Bsihoje {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JanelaPrincipal j = new JanelaPrincipal();
+        j.setVisible(true);
     }
     
 }
